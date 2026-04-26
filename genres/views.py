31395ca1a08b2e07850cddfc262353fae5c50e62
lphillipe <luisphillipe200@gmail.com>
2026-04-26 20:17:00 -0300

    Add CSRF exemption to genre_view and ensure genre creation in POST request

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,9 @@
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from genres.models import Genre
 
-
+@csrf_exempt
 def genre_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
@@ -10,3 +11,6 @@ def genre_view(request):
         return JsonResponse(data, safe=False)
     elif request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+        return JsonResponse({'id': new_genre.id,'name': new_genre.name}, status=201,)
