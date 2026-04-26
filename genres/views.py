bd34845e5b17073830e33008b3a56437ef948ad9
lphillipe <luisphillipe200@gmail.com>
2026-04-26 20:07:39 -0300

    Refactor genre_view to handle GET and POST requests

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,12 @@
+import json
 from django.http import JsonResponse
 from genres.models import Genre
 
 
 def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
