f4fceed895a4a9c3f348363466b743bcb3a92f6b
lphillipe <luisphillipe200@gmail.com>
2026-04-27 18:55:54 -0300

    Refactor genre view to separate create/list and detail functionalities

--- genres/views.py
+++ genres/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from genres.models import Genre
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
@@ -14,3 +14,9 @@ def genre_view(request):
         new_genre = Genre(name=data['name'])
         new_genre.save()
         return JsonResponse({'id': new_genre.id,'name': new_genre.name}, status=201,)
+    
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = Genre.objects.get(pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)
