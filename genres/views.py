5bb6f7697270b180c7cbe9f80f8cb39e09887a2e
lphillipe <luisphillipe200@gmail.com>
2026-04-28 12:06:01 -0300

    Implement PUT method in genre_detail_view for updating genre names

--- genres/views.py
+++ genres/views.py
@@ -10,6 +10,7 @@ def genre_create_list_view(request):
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
         return JsonResponse(data, safe=False)
+    
     elif request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         new_genre = Genre(name=data['name'])
@@ -20,6 +21,14 @@ def genre_create_list_view(request):
 def genre_detail_view(request, pk):
     genre = get_object_or_404(Genre, pk=pk)
 
-
-    data = {'id': genre.id, 'name': genre.name}
-    return JsonResponse(data)
+    if request.method == 'GET':
+        data = {'id': genre.id, 'name': genre.name}
+        return JsonResponse(data)
+    
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse(
+            {'id': genre.id, 'name': genre.name}
+        )
