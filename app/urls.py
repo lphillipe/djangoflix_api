7362aed63c519cca06b74ea7fc243e775bbca2f4
lphillipe <luisphillipe200@gmail.com>
2026-04-30 18:41:23 -0300

    Refactor API URLs to include actor, movie, and review endpoints

--- app/urls.py
+++ app/urls.py
@@ -10,13 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/', include('genres.urls')),
-    
-    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int>:pk/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
-
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
+    path('api/v1', include('actors.urls')),
+    path('api/v1', include('movies.urls')),
+    path('api/v1', include('reviews.urls'))
 ]
