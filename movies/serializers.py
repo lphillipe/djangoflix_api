fda28e6941838c3abdc0f0d3ee4fe122434cedb3
lphillipe <luisphillipe200@gmail.com>
2026-05-14 11:57:21 -0300

    Refactor Movie serializer and views to enforce release date validation and update serializer class name

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 from movies.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        return value
