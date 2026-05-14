23a827b8ab93eb26149f42d703e9449b08a2a568
lphillipe <luisphillipe200@gmail.com>
2026-05-14 15:51:17 -0300

    Fix validation logic for release date and resume length in MovieModelSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -13,11 +13,11 @@ class MovieModelSerializer(serializers.ModelSerializer):
         return 5
 
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
         return value
