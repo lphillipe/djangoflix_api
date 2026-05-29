7a85bc8008192c3de3ed43d96ba4d4f5896a09aa
lphillipe <luisphillipe200@gmail.com>
2026-05-29 07:28:55 -0300

    Add MovieStatsSerializer and enhance MovieStatsView to return detailed movie statistics

--- movies/serializers.py
+++ movies/serializers.py
@@ -28,3 +28,9 @@ class MovieModelSerializer(serializers.ModelSerializer):
         if len(value) > 500:
             raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
         return value
+    
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    average_stars = serializers.FloatField()
--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
-from movies.serializers import MovieModelSerializer
+from movies.serializers import MovieModelSerializer, MovieStatsSerializer
 from reviews.models import Review
 
 
@@ -29,10 +29,18 @@ class MovieStatsView(views.APIView):
         movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
         total_reviews = Review.objects.count()
         average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
-        
-        return response.Response(data={
+
+        data= {
             'total_movies': total_movies,
             'movies_by_genre': movies_by_genre,
             'total_reviews': total_reviews,
             'average_stars': round(average_stars, 1) if average_stars else 0,
-        }, status=status.HTTP_200_OK)
+        }
+
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        
+        return response.Response(
+            data=serializer.validated_data, 
+            status=status.HTTP_200_OK
+        )
